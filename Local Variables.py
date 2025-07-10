c75d297d7d6b7670e94fb80aa8fb2691a37c6297
se-hyeonkim <michael_0903@naver.com>
2025-07-10 15:46:05 +0900

    Create Local Variables.py

---
+++ Local Variables.py
@@ -0,0 +1,19 @@
+'''아래 함수 실행 후 step1, step2, step3 결과를 예측 해보세요.
+
+a = 20
+
+def test():
+    # 지역변수
+    a = 35
+    return a
+
+print('step1 : ', a)
+
+a = 100
+
+print('step2 : ', a)
+print('step3 : ', test())'''
+
+step1 :  20
+step2 :  100
+step3 :  35
